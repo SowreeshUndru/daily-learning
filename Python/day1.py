779499a1ba7e7041a0b6b04c9274d6beff82844e
Sowreesh Undru <undrusowreesh@gmail.com>
2026-07-17 17:43:54 +0000

    python

--- Python/day1.py
+++ Python/day1.py
@@ -9,7 +9,7 @@ string=input("type name : ")
 print(string[0:3]) #[i:j] then [i,j)  
 print(string[0:]) #[i:j] then [i,j) // here same as [0:size of string]   
 print(string[:3]) #[i:j] then [i,j)  //here is same as [0:3]
-#negative index 
+#negative index .
 
 
 
