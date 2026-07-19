16dc3e740ba90f3753da83099c1074724ddd9aed
sowreeshundru <undrusowreesh@gmail.com>
2026-07-19 17:44:29 +0000

    pyton list and dict and js and it objects

---
+++ JS/object.js
@@ -0,0 +1,7 @@
+var obj={
+    "value":25
+}
+
+console.log(obj["value"]); // output : 25
+console.log(obj.value); // output : 25
+//console.log(obj[value]); // output : ERROR (name is not defined)
---
+++ Python/DictornaryandSets.py
@@ -0,0 +1,11 @@
+dict={
+    12:"Lokesh",
+    12.3:"Ramesh",
+    "name":"sowreesh",
+    (1,2):"rajesh",
+    
+    #[1,2]:"jamil",
+}
+print(dict)
+print(dict["name"])
+print(dict[name])
